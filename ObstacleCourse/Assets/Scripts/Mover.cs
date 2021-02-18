6cf65497b95d5531496f9e472ecaea33a48647ed
DESKTOP\\VVRON <me@aaronakrong.com>
2021-02-18 15:49:40 +0000

    First Commit

---
+++ ObstacleCourse/Assets/Scripts/Mover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField]
+    float zValue = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xValue = Input.GetAxis("Horizontal");
+        float yValue = Input.GetAxis("Vertical");
+        transform.Translate(xValue, yValue, zValue);
+    }
+}
